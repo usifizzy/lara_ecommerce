46628456a4abdfb32c28b8edb49f3573c2540ec8
usifizzy <usifizzy@yahoo.com>
2024-04-12 05:40:14 +0100

    product table migration created

---
+++ database/migrations/2024_04_12_043257_create_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 128);
+            $table->double('price', 38, 2)->default(0);
+            $table->string('category', 64);
+            $table->string('description', 5000)->nullable();
+            // $table->enum('fee_bearer', ['client', 'customer'])->default('client');
+            $table->string('image', 128);
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};

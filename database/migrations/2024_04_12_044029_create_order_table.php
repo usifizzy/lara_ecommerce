23f07bc7ebcb55f6ee14ab76a0791404228f58fc
usifizzy <usifizzy@yahoo.com>
2024-04-12 05:45:08 +0100

    order table migration created

---
+++ database/migrations/2024_04_12_044029_create_order_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //     protected $fillable    = ['order_no', 'amount', 'status', 'customer_id'];
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('order_no', 128);
+            $table->double('amount', 38, 2)->default(0);
+            $table->string('category', 64);
+            $table->enum('status', [ 'pending', 'paid', 'delivered'])->default('pending');
+            
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order');
+    }
+};
